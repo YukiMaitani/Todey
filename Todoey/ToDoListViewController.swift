48e82946b618bbccab5fafe2612c9d89f5e65740
YukiMaitani <fightzoi1997@gmail.com>
2022-06-21 19:48:17 +0900

    セルをタッチした時にチェックマーク

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -35,4 +35,15 @@ class ToDoListViewController: UITableViewController {
         }
         return cell
     }
+    
+    //MARK: - Tabeleview Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+    }
 }
